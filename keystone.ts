c98268439c170fc5a88308950e36e282adabeb72
Neil Coutinho <njcoutinho@gmail.com>
2021-08-25 08:39:35 +0530

    Keystone session config

--- keystone.ts
+++ keystone.ts
@@ -1,28 +1,52 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
 
 const databaseURL =
-    process.env.DATABASE_URL || 'mongodb://localhost/store-database';
+  process.env.DATABASE_URL || 'mongodb://localhost/store-database';
 
 const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 30, // sec * min * hrs * days
-    secret: process.env.COOKIE_SECRET,
+  maxAge: 60 * 60 * 24 * 30, // sec * min * hrs * days
+  secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-    // @ts-ignore
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['email', 'password', 'name'],
+  },
+});
+
+export default withAuth(
+  config({
     server: {
-        cors: {
-            origin: [process.env.FRONTEND_URL],
-            credentials: true,
-        },
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
     db: {
-        adapter: 'mongoose',
-        url: databaseURL,
+      adapter: 'mongoose',
+      url: databaseURL,
     },
-    lists: createSchema({}),
+    lists: createSchema({
+      User,
+    }),
     ui: {
-        isAccessAllowed: () => true, // change later
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
     },
-});
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id name email',
+    }),
+  })
+);
