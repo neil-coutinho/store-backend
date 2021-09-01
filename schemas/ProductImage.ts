5a6da6d53999dbb0e1fedbae5fec33628014e062
Neil Coutinho <njcoutinho@gmail.com>
2021-09-01 09:05:01 +0530

    Cloudinary config setup. Created schema ProductImage

---
+++ schemas/ProductImage.ts
@@ -0,0 +1,20 @@
+import { text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+import { cloudinaryImage } from '@keystone-next/cloudinary';
+
+export const cloudinaryConfig = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'images',
+};
+
+export const ProductImage = list({
+  fields: {
+    image: cloudinaryImage({
+      label: 'Source',
+      cloudinary: cloudinaryConfig,
+    }),
+    altText: text(),
+  },
+});
